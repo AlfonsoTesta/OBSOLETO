a1c418a1f912636409f6efee7249a1f00b2241e9
AlfonsoTesta <alfonsotestabonino@gmail.com>
2022-06-15 07:28:24 -0300

    Agregar mas funciones al registro

--- login.php
+++ login.php
@@ -5,10 +5,10 @@
             <div class="close" onclick="closeLog('bg_b')">+</div>
             <img src="imagenes/logo/logo.png" class="logo">
             <form action="">
-                <input type="text" class="text Nam" id="nombre" placeholder="Name/Email">
+                <input type="text" class="text Nam" value="AAA@gmail.com" id="nombre" placeholder="Name/Email">
                 <a href="" id="recu">Forgot password?</a>
                 
-                <input type="password" class="text Pswd" id="pswd" placeholder="Password">
+                <input type="password"  value="AAA" class="text Pswd" id="pswd" placeholder="Password">
                 
                 <input type="button" value="SUBMIT" onclick="login()"  class="botones" > 
             </form>
@@ -19,13 +19,13 @@
             <div class="close" onclick="closeLog('bg_b')">+</div>
             <img src="imagenes/logo/logo.png" class="logo">
             <form action="">
-                <input type="text" class="text Nam" id="nombre" placeholder="Full Name">
-                <input type="email"id='mail' name="Email" class="text em" placeholder="Email">
-                <input type="date" id='cumple' name="Birthday" class="text date">
-                <input type="password" class="text regp" id="contra" placeholder="Password">
-                <input type="password" class="text regp" id="confi" placeholder="Confirm Password">
+                <input type="text" class="text Nam" value="AAA" id="nombre" placeholder="Full Name">
+                <input type="email"id='mail' name="Email" value="AAA@gmail.com" class="text em" placeholder="Email">
+                <input type="date" id='cumple' name="Birthday" value="AAA" class="text date">
+                <input type="password" class="text regp" id="contra" value="AAA" placeholder="Password">
+                <input type="password" class="text regp" id="confi" value="AAA" placeholder="Confirm Password">
 
-                <input type="submit" value="SUBMIT" onclick="register()"  class="botones" > 
+                <input type="button" value="SUBMIT" onclick="register()"  class="botones" > 
             </form>
         </div>
         <div class="signUp" id="log"><p>You don't have an account? Register <a onclick="replaceLog('loginH' ,'registerH');changer('log','reg');">HERE</a></p></div>
